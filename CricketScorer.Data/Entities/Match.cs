7703302cc5cb44cfdd8057a728d6a81ca3a109d9
Matt Holland <mattholland0202@gmail.com>
2016-10-17 21:58:17 +0100

    Entities

---
+++ CricketScorer.Data/Entities/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CricketScorer.Data.Entities
+{
+    public class Match : IVersionedEntity
+    {
+        public byte[] Version { get; set;}
+
+        public DateTime MatchDate;
+    }
+}
